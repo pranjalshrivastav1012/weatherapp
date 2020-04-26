ec69dc966b22ac3df81dc5bc45495e2ea433469b
Ankit Srivastava <ankit.s@commerceiq.ai>
2020-04-27 02:11:42 +0530

    running response

--- app/src/main/java/com/pranjal/sri/weatherapp/MainActivity.java
+++ app/src/main/java/com/pranjal/sri/weatherapp/MainActivity.java
@@ -10,48 +10,73 @@ import android.widget.TextView;
 
 import android.util.Log;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pranjal.sri.weatherapp.api.response.DataResponse;
 import com.pranjal.sri.weatherapp.api.response.Weather;
 
-public class MainActivity extends Activity implements LocationListener{
-    protected LocationManager locationManager;
-    protected LocationListener locationListener;
-    protected Context context;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends Activity {
+
     TextView txtLat;
-    String lat;
-    String provider;
-    protected String latitude,longitude;
-    protected boolean gps_enabled,network_enabled;
+
+    private final String appid = "ff512bbf2e8833415bb66c6427eab63e";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setDataResponse(30.35f, 76.77f, appid);
+    }
+
+    private void setDataResponse(float lat, float lon, String appId){
         txtLat = (TextView) findViewById(R.id.textview1);
+        String url ="https://api.openweathermap.org/data/2.5/weather?lat=" +
+                lat + "&lon=" + lon + "&appid=" + appId;
 
-//        Weather wthr = new Weather(1, "abc", "xyz", "lol");
-//        System.out.println(wthr.getId());
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-    }
-    @Override
-    public void onLocationChanged(Location location) {
-        txtLat = (TextView) findViewById(R.id.textview1);
-        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
-    }
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
 
-    @Override
-    public void onProviderDisabled(String provider) {
-        Log.d("Latitude","disable");
-    }
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
 
-    @Override
-    public void onProviderEnabled(String provider) {
-        Log.d("Latitude","enable");
-    }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    final String resp = response.body().string();
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        Log.d("Latitude","status");
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            txtLat.setText(resp);
+                            try{
+                                txtLat.setText(resp);
+                            } catch (Exception ignored) {
+
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
